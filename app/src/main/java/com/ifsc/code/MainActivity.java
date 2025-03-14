13d33e6a9e99d52680032bf9394c5a340f9ae782
AMelloDev <demelloamnda3@gmail.com>
2025-03-13 22:19:44 -0300

    Sorteador

--- app/src/main/java/com/ifsc/code/MainActivity.java
+++ app/src/main/java/com/ifsc/code/MainActivity.java
@@ -3,35 +3,37 @@ package com.ifsc.code;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edMin, edMax;
+    TextView TxResult;
 
-
-
-    int contador=0;
-    Button button;
-    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button);
-        textView =findViewById(R.id.textView);
+     super.onCreate(savedInstanceState);
+     setContentView(R.layout.activity_main);
+     edMin = findViewById(R.id.edMin);
+     edMax = findViewById(R.id.edMax);
+     TxResult = findViewById(R.id.TxResult);
+     Button b = findViewById(R.id.btSort);
 
-       button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                textView.setText(Integer.toString(contador));
-                contador++;
-            }
-        });
+     b.setOnClickListener(v -> {
+         int min = Integer.parseInt(edMin.getText().toString());
+         int max = Integer.parseInt(edMax.getText().toString());
+
+         //Random
+         Random random = new Random();
+
+         int n= random.nextInt(max-min)+min;
+         TxResult.setText(Integer.toString(n));
+     });
+
 
     }
 
