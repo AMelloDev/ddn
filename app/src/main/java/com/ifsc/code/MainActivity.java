864087eb2345fcbe40b7358608b1baca1e32839c
amanda <amanda>
2025-05-15 20:03:06 -0300

    listview implementado com strings

--- app/src/main/java/com/ifsc/code/MainActivity.java
+++ app/src/main/java/com/ifsc/code/MainActivity.java
@@ -2,7 +2,10 @@ package com.ifsc.code;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,28 +14,32 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.ifsc.code.*;
+import com.ifsc.myapplication.R;
+
 public class MainActivity extends AppCompatActivity {
 
+    ListView lv;
+    String[] nomes =new String[]{"pera", "uva", "maça", "goiaba", "morango", "abacaxi", "kiwi"};
 
-
-    int contador=0;
-    Button button;
-    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button);
-        textView =findViewById(R.id.textView);
+        setContentView(com.ifsc.myapplication.R.layout.activity_main);
+        lv = findViewById(R.id.listview);
 
-       button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                textView.setText(Integer.toString(contador));
-                contador++;
-            }
-        });
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                nomes);
 
+        lv.setAdapter(adapter);
+
+       /* lv.setOnClickListener(new AdapterView.OnClickListener(
+                @Override
+                p
+
+        ));*/
     }
 
 }
